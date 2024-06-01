c2600e540a207ce5d8198894a68623b99a5ea8ef
BalramMardi <120470787+BalramMardi@users.noreply.github.com>
2024-06-01 14:36:29 +0530

    Time: 4 ms (41.37%) | Memory: 10.2 MB (14.74%) - LeetSync

---
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    private:
+    map<string, string> encodeMap;
+    map<string, string> decodeMap;
+        string base = "http://tinyurl.com/";
+    // Encodes a URL to a shortened URL.
+    public:
+    string encode(string longUrl) {
+        if(!encodeMap.count(longUrl)){
+            string shortUrl = base + to_string(encodeMap.size()+1);
+            encodeMap[longUrl]=shortUrl;
+            decodeMap[shortUrl]=longUrl;
+
+        }
+        return encodeMap[longUrl];
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return decodeMap[shortUrl];
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
