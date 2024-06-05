2f42c32d98e23c48073691a584d69ae095525b22
BalramMardi <120470787+BalramMardi@users.noreply.github.com>
2024-06-05 22:49:12 +0530

    Time: 18 ms (67.97%) | Memory: 29.5 MB (83.35%) - LeetSync

--- 665-non-decreasing-array/non-decreasing-array.cpp
+++ 665-non-decreasing-array/non-decreasing-array.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    bool checkPossibility(vector<int>& nums) 
-    {
-        int n = nums.size(), flag = true;
-        for(int i=0;i<n-1;i++)
-        {
-            if(nums[i] > nums[i+1])
-            {
-                if(flag) 
-                {
-                    flag = false;
-                    if(i+2 < n)
-                    {
-                        if(nums[i] < nums[i+2]) nums[i+1] = nums[i];
-                        else nums[i] = nums[i+1];
-                    }
-                    else nums[i+1] = nums[i];
+    bool checkPossibility(vector<int>& nums) {
+        int pos = -1;
+        int n = nums.size();
+        
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                if (pos != -1) {
+                    return false;
                 }
-                else return false;
+                pos = i;
             }
         }
-        vector<int>temp = nums;
-        sort(nums.begin(), nums.end());
-        return nums == temp;
+
+        // If no pair (nums[i], nums[i+1]) found that violates the non-decreasing order
+        if (pos == -1) return true;
+
+        // If violation is at the start or at the end of the array
+        if (pos == 0 || pos == n - 2) return true;
+
+        // Check if modifying nums[pos] or nums[pos+1] would make the array non-decreasing
+        if (nums[pos - 1] <= nums[pos + 1] || nums[pos] <= nums[pos + 2]) return true;
+
+        return false;
     }
 };
