0cc8eee4fe30687e395aa9798bddfe901e4780ce
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2024-07-18 19:58:53 +0530

    Time: 14 ms (24.55%) | Memory: 11.1 MB (18.79%) - LeetSync

---
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int memo[501];
+    int solve(vector<int>& arr,int i,int k){
+        if(i >= arr.size()){
+            return 0;
+        }
+
+        if(memo[i]!=-1){
+            return memo[i];
+        }
+
+        int result=0;
+        int currmax=-1;
+
+        for(int j=i;j<arr.size() && j-i+1 <= k ;j++){
+            currmax = max(currmax, arr[j]);
+            result = max(result , (j-i+1)*currmax + solve(arr,j+1,k));
+        }
+
+        return memo[i]= result;
+
+    }
+
+
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+
+        memset(memo,-1,sizeof(memo));
+        return solve(arr,0,k);
+    }
+};
