eee0d5a0fbaa0a6530554827f14c7f42460c706b
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2024-07-22 20:08:00 +0530

    Time: 15 ms (36.31%) | Memory: 15.4 MB (76.12%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int memo[301][5001];
+    int solve(vector<int>& coins,int total,int idx){
+        if(total < 0 or idx>=coins.size()){
+            return 0;
+        }
+
+        if(total == 0){
+            return 1;
+        }
+
+        if(memo[idx][total] != -1){
+            return memo[idx][total];
+        }
+
+        if(total < coins[idx]){
+            return memo[idx][total]= solve(coins,total,idx+1);
+        }
+
+        int take = solve(coins,total-coins[idx],idx);
+        int skip = solve(coins,total,idx+1);
+
+        return  memo[idx][total]= take+skip;
+
+
+    }
+
+
+    int change(int amount, vector<int>& coins) {
+        memset(memo,-1,sizeof(memo));
+        return solve(coins,amount,0);
+    }
+};
