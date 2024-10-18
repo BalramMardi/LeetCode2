1996a0abe9fba48853cea62b6437d5f8a976ef27
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2024-10-18 10:50:10 +0530

    Time: 12 ms (100.00%) | Memory: 100.2 MB (88.38%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
@@ -1,19 +1,27 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void dfs(int u, vector<int>& memo, vector<int>& edges) {
+    int time = 0;
+    while (u != -1 && memo[u] == -1) {
+        memo[u] = time++;
+        u = edges[u];
+    }
+}
+
 class Solution {
 public:
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
-        int n = size(edges);
-        vector<int> m1(n,-1), m2(n, -1);
-        auto dfs = [&edges](int u, auto & memo)->void{
-            int time = 0;
-            while(u != -1 && memo[u] == -1){
-                memo[u] = time++;
-                u = edges[u];
-            }
-        };
-        dfs(node1, m1), dfs(node2, m2);
+        int n = edges.size();
+        vector<int> m1(n, -1), m2(n, -1);
+
+        dfs(node1, m1, edges);
+        dfs(node2, m2, edges);
+
         int id = -1, minMax = 1e9;
-        for(int i=0; i<n; i++){
-            if(min(m1[i], m2[i]) > -1 && max(m1[i], m2[i]) < minMax){
+        for (int i = 0; i < n; i++) {
+            if (min(m1[i], m2[i]) != -1 && max(m1[i], m2[i]) < minMax) {
                 minMax = max(m1[i], m2[i]);
                 id = i;
             }
