4c68b1a57f532dd5052a99b272e2954dc8c3f63e
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2024-10-29 23:38:12 +0530

    Time: 4 ms (84.35%) | Memory: 98.1 MB (76.52%) - LeetSync

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
-    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        
-        vector<vector<int>>adj(n);
-        for(int i=0;i<edges.size();i++){
-            int u=edges[i][0]; int v = edges[i][1];
-            adj[u].push_back(v);
-        }
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {  
         vector<int> inorder(n,0);
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<adj[i].size();j++){
-                inorder[adj[i][j]]++;
-            }
+        for(int i=0;i<edges.size();i++){
+           inorder[edges[i][1]]++;
         }
-        
         vector<int> ans;
         for(int i=0;i<n;i++){
             if(inorder[i]==0){
