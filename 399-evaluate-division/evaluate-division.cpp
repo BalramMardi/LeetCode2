d66e6fb09457e8e1fc830df1a50e1e939687ed5f
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2024-10-30 11:02:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (66.35%) - LeetSync

---
+++ 399-evaluate-division/evaluate-division.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    unordered_map<string,unordered_map<string,double>>mp;
+    void buildGraph(vector<vector<string>>& eq,vector<double>& values){
+        for(int i=0;i<eq.size();i++){
+            string A = eq[i][0];
+            string B = eq[i][1];
+            mp[A][B] = values[i];
+            mp[B][A] = 1.0/values[i];
+        }
+    }
+    double dfs(string start,string &end,unordered_map<string,bool>&vis){
+        if(mp.find(start)==mp.end() or mp.find(end)==mp.end()){
+            return -1.0;
+        }
+        if(start==end){
+            return 1.0;
+        }
+
+        vis[start]=true;
+
+        for(auto &neigh : mp[start]){
+            if(vis[neigh.first]==false){
+                double result = dfs(neigh.first,end,vis);
+                if(result != -1.0){
+                    return result * neigh.second;
+                }
+            }
+        }
+
+        return -1.0;
+    }
+
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        buildGraph(equations,values);
+        vector<double> res;
+        for(int i=0;i<queries.size();i++){
+            string start = queries[i][0];
+            string end = queries[i][1];
+            unordered_map<string,bool> vis;
+            double ans = dfs(start,end,vis);
+            res.push_back(ans);
+        }
+        return res;
+    }
+};
