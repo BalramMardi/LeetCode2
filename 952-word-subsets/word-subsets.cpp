6d1ea2831e4431e0de370fbeb7bb274df79ac942
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-01-10 21:50:32 +0530

    Time: 44 ms (70.78%) | Memory: 84.3 MB (72.15%) - LeetSync

--- 952-word-subsets/word-subsets.cpp
+++ 952-word-subsets/word-subsets.cpp
@@ -1,5 +1,48 @@
 class Solution {
 public:
+    bool isSubset(vector<int>& freq2, vector<int>& temp) {
+        for(int i = 0; i < 26; i++) {
+            if(temp[i] < freq2[i]) {
+                return false; 
+            }
+        }
+        return true;
+    }
+
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<string> result;
+
+        vector<int> freq2(26);
+     
+        for(string &word : words2) { 
+            int temp[26] = {0};
+
+            for(char &ch : word) {
+                temp[ch-'a']++;
+
+                freq2[ch-'a'] = max(freq2[ch-'a'], temp[ch-'a']);
+            }
+        }
+  
+        for(string &word : words1) {
+            vector<int> temp(26, 0);
+
+            for(char &ch : word) { 
+                temp[ch-'a']++;
+            }
+
+            if(isSubset(freq2, temp) == true) { 
+                result.push_back(word);
+            }
+        }
+
+        return result;
+    }
+};
+
+
+/* class Solution {
+public:
 
 
     vector<int> count(const string& S) {
@@ -36,4 +79,4 @@ public:
 
         return ans;
     }
-};
+}; */
