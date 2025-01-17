9db90a1f76a6321c9bb50ecb209048fdedc67c74
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-01-18 02:36:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (79.49%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
     int memo[501];
-    int solve(vector<int>& arr,int i,int k){
-        if(i >= arr.size()){
+    int solve(vector<int>& arr, int i, int k) {
+        if (i >= arr.size()) {
             return 0;
         }
 
-        if(memo[i]!=-1){
+        if (memo[i] != -1) {
             return memo[i];
         }
 
-        int result=0;
-        int currmax=-1;
+        int result = 0;
+        int currmax = -1;
 
-        for(int j=i;j<arr.size() && j-i+1 <= k ;j++){
+        for (int j = i; j < arr.size() && j - i + 1 <= k; j++) {
             currmax = max(currmax, arr[j]);
-            result = max(result , (j-i+1)*currmax + solve(arr,j+1,k));
+            result = max(result, (j - i + 1) * currmax + solve(arr, j + 1, k));
         }
 
-        return memo[i]= result;
-
+        return memo[i] = result;
     }
-
 
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
 
-        memset(memo,-1,sizeof(memo));
-        return solve(arr,0,k);
+        // memset(memo,-1,sizeof(memo));
+        // return solve(arr,0,k);
+
+        int n = arr.size();
+        vector<int> dp(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; --i) {
+            int maxVal = 0, maxSum = 0;
+
+            for (int j = 0; j < k && i + j < n; ++j) {
+                maxVal = max(maxVal, arr[i + j]);
+                maxSum = max(maxSum, maxVal * (j + 1) + dp[i + j + 1]);
+            }
+
+            dp[i] = maxSum;
+        }
+
+        return dp[0];
     }
 };
