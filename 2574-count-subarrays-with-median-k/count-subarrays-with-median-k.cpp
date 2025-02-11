67264727e6a6136a7c2a37f3a8d3a8e00c5e3846
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-02-11 23:31:11 +0530

    Time: 22 ms (81.75%) | Memory: 53.2 MB (86.90%) - LeetSync

--- 2574-count-subarrays-with-median-k/count-subarrays-with-median-k.cpp
+++ 2574-count-subarrays-with-median-k/count-subarrays-with-median-k.cpp
@@ -1,25 +1,57 @@
 class Solution {
 public:
     int countSubarrays(vector<int>& nums, int k) {
-        unordered_map<int, int> balanceCount;
-        int pivotIndex = find(nums.begin(), nums.end(), k) - nums.begin();
+        int n = nums.size();
         int result = 0;
 
-        for (int i = pivotIndex, balance = 0; i < nums.size(); ++i) {
-            if (nums[i] > k)
-                balance += 1;
-            else if (nums[i] < k)
-                balance -= 1;
-            ++balanceCount[balance];
+        int kIndex = -1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == k) {
+                kIndex = i;
+                break;
+            }
+        }
+        if (kIndex == -1) return 0;
+
+       
+        vector<int> transformed(n);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > k) {
+                transformed[i] = 1;
+            } else if (nums[i] < k) {
+                transformed[i] = -1;
+            } else {
+                transformed[i] = 0;
+            }
         }
 
-        for (int i = pivotIndex, balance = 0; i >= 0; --i) {
-            if (nums[i] > k)
-                balance += 1;
-            else if (nums[i] < k)
-                balance -= 1;
+     
+        unordered_map<int, int> prefix_counts;
+        prefix_counts[0] = 1;
+        int prefix_sum = 0;
+        bool kFound = false;
 
-            result += balanceCount[-balance] + balanceCount[-balance + 1];
+        for (int i = 0; i < n; i++) {
+            prefix_sum += transformed[i];
+
+     
+            if (nums[i] == k) {
+                kFound = true;
+            }
+
+            if (kFound) {
+                if (prefix_counts.find(prefix_sum) != prefix_counts.end()) {
+                    result += prefix_counts[prefix_sum];
+                }
+
+                if (prefix_counts.find(prefix_sum - 1) != prefix_counts.end()) {
+                    result += prefix_counts[prefix_sum - 1];
+                }
+            }
+
+            if (!kFound) {
+                prefix_counts[prefix_sum]++;
+            }
         }
 
         return result;
