19e35f63c18c15dd5e8705e574857b6487415140
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-02-13 01:21:04 +0530

    Time: 3 ms (28.80%) | Memory: 97.4 MB (60.77%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -7,9 +7,6 @@ public:
         int op=0;
 
         for(int i=0;i<prices.size();i++){
-            if(lsf > prices[i]){
-                lsf = prices[i];
-            }
 
             lsf = min(lsf, prices[i]);
 
