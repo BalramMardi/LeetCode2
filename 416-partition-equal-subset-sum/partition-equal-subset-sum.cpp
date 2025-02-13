b80c147c5d147eaef0b0a22c93783fe2ab157c2f
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-02-13 13:37:54 +0530

    Time: 115 ms (61.94%) | Memory: 16.9 MB (69.37%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -12,51 +12,33 @@
 
 class Solution {
 public:
-    bool canPartitionHelper(const std::vector<int> &nums, int n, int sum)
-{
-    // Create a 2D array to store the results of subproblems
-    std::vector<std::vector<bool>> dp(n + 1, std::vector<bool>(sum + 1, false));
+    bool canPartitionHelper(const vector<int>& nums, int n, int sum) {
+        vector<vector<bool>> dp(n + 1,vector<bool>(sum + 1, false));
 
-    // Initialize the first column as true (0 sum is possible with any subset)
-    for (int i = 0; i <= n; i++)
-    {
-        dp[i][0] = true;
-    }
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
 
-    // Fill the partition table
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= sum; j++)
-        {
-            // If the current element is greater than the sum, we cannot include it
-            if (nums[i - 1] > j)
-            {
-                dp[i][j] = dp[i - 1][j];
-            }
-            else
-            {
-                // Otherwise, we check if the sum can be obtained by any of:
-                // (i) excluding the current element
-                // (ii) including the current element
-                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (nums[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                }
             }
         }
+
+        return dp[n][sum];
     }
 
-    return dp[n][sum];
-}
+    bool canPartition(const vector<int>& nums) {
+        int total_sum = accumulate(nums.begin(), nums.end(), 0);
 
-// Function to determine if the array can be partitioned into two subsets with equal sum
-bool canPartition(const std::vector<int> &nums)
-{
-    int total_sum = std::accumulate(nums.begin(), nums.end(), 0);
+        if (total_sum % 2 != 0)
+            return false;
 
-    // If the total sum is odd, it's not possible to partition it into two equal subsets
-    if (total_sum % 2 != 0)
-        return false;
-
-    // Find if there is a subset with sum equal to half of the total sum
-    int subset_sum = total_sum / 2;
-    return canPartitionHelper(nums, nums.size(), subset_sum);
-}
+        int subset_sum = total_sum / 2;
+        return canPartitionHelper(nums, nums.size(), subset_sum);
+    }
 };
