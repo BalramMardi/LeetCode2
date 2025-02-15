2ee9d532a038a0ae2febd4ce4bae9e7c070dca2a
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-02-15 19:59:55 +0530

    Time: 20 ms (48.88%) | Memory: 64.3 MB (24.28%) - LeetSync

--- 3391-maximum-difference-score-in-a-grid/maximum-difference-score-in-a-grid.cpp
+++ 3391-maximum-difference-score-in-a-grid/maximum-difference-score-in-a-grid.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+    int helper(vector<vector<int>>& grid, int i, int j, int& ans, vector<vector<int>>& dp) {
+        if (i == 0 && j == 0) 
+            return grid[i][j]; 
+        
+        if (dp[i][j] != INT_MAX) 
+            return dp[i][j];
+        
+        int mn = INT_MAX;
+        if (i > 0) 
+            mn = min(mn, helper(grid, i - 1, j, ans, dp)); 
+        if (j > 0) 
+            mn = min(mn, helper(grid, i, j - 1, ans, dp)); 
+        ans = max(ans, grid[i][j] - mn); 
+        return dp[i][j] = min(grid[i][j], mn); 
+    }
+
+    int maxScore(vector<vector<int>>& grid) {
+        int ans = INT_MIN;
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, INT_MAX)); 
+        helper(grid, m - 1, n - 1, ans, dp);
+        return ans;
+    }
+};
+
+
+
+
+/* class Solution {
+public:
     int maxScore(vector<vector<int>>& grid) {
         int ans = INT_MIN;
         for (int i = 0; i < grid.size(); ++i) {
@@ -17,4 +47,4 @@ public:
         }
         return ans;
     }
-};
+}; */
