01f570aeeb028f387955cf9a33b5bc30c0a265a2
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-02-19 11:13:02 +0530

    Time: 1 ms (79.30%) | Memory: 7.7 MB (99.21%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,29 +1,75 @@
-class Solution {
+/* class Solution {
 public:
-    string getHappyString(int n, int k) {
-        string currentString = "";
-        vector<string> happyStrings;
-        generateHappyStrings(n, currentString, happyStrings);
-
-        if (happyStrings.size() < k) return "";
-
-        sort(happyStrings.begin(), happyStrings.end());
-
-        return happyStrings[k - 1];
-    }
-
-private:
-    void generateHappyStrings(int n, string currentString,vector<string> &happyStrings) {
-        if (currentString.size() == n) {
-            happyStrings.push_back(currentString);
+    void solve(int n, string &curr, vector<string> &result) {
+        if (curr.length() == n) {
+            result.push_back(curr);
             return;
         }
-
-        for (char currentChar = 'a'; currentChar <= 'c'; currentChar++) {
-            if (currentString.size() > 0 && currentString.back() == currentChar)
+        
+        for (char ch = 'a'; ch <= 'c'; ch++) {
+            if (!curr.empty() && curr.back() == ch)
                 continue;
+            
+            // Do
+            curr.push_back(ch);
 
-            generateHappyStrings(n, currentString + currentChar, happyStrings);
+            // Explore
+            solve(n, curr, result);
+
+            // Undo
+            curr.pop_back();
         }
+    }
+
+    string getHappyString(int n, int k) {
+        string curr = "";
+        vector<string> result;
+        solve(n, curr, result);
+
+        if (result.size() < k) 
+            return "";
+        
+        return result[k - 1];
+    }
+}; */
+
+
+class Solution {
+public:
+    void solve(int n, string &curr, int &count, int k, string &result) {
+        if (curr.length() == n) {
+            count++;
+            if (count == k) {
+                result = curr; 
+            }
+            return;
+        }
+        
+        for (char ch = 'a'; ch <= 'c'; ch++) {
+            if (!curr.empty() && curr.back() == ch)
+                continue;
+            
+     
+            curr.push_back(ch);
+
+      
+            solve(n, curr, count, k, result);
+
+       
+            if (!result.empty()) return;
+
+        
+            curr.pop_back();
+        }
+    }
+
+    string getHappyString(int n, int k) {
+        string curr = "";
+        string result = "";
+        int count = 0;
+        
+        solve(n, curr, count, k, result);
+
+        return result;
     }
 };
