524cf739a7852ac9bfd08f51283a3bb670283119
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-02-22 20:09:35 +0530

    Time: 15 ms (74.72%) | Memory: 34.7 MB (75.84%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        queue<pair<int, int>> q;
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (mat[i][j] == 0) {
+                    dist[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+
+        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        while (!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+
+            for (auto [dr, dc] : directions) {
+                int nr = r + dr;
+                int nc = c + dc;
+
+             
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n &&
+                    dist[nr][nc] > dist[r][c] + 1) {
+                    dist[nr][nc] = dist[r][c] + 1;
+                    q.push({nr, nc});
+                }
+            }
+        }
+
+        return dist;
+    }
+};
