468d39ca54fb1a6b1c8a1a21b127a3510ae3f14f
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-02-23 00:04:16 +0530

    Time: 29 ms (40.63%) | Memory: 23.7 MB (76.80%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,4 +1,48 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
 class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& g) {
+        int n = g.size();
+        if (g[0][0] != 0 || g[n - 1][n - 1] != 0) return -1;
+
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
+
+        vector<pair<int, int>> directions = {
+            {1, 0}, {-1, 0}, {0, 1}, {0, -1},
+            {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
+        };
+
+        pq.push({1, {0, 0}}); // {distance, {i, j}}
+        dist[0][0] = 1;
+
+        while (!pq.empty()) {
+            auto [d, cell] = pq.top();
+            auto [i, j] = cell;
+            pq.pop();
+
+            if (i == n - 1 && j == n - 1) return d;
+
+            for (auto [di, dj] : directions) {
+                int ni = i + di, nj = j + dj;
+
+                if (ni >= 0 && ni < n && nj >= 0 && nj < n && g[ni][nj] == 0 && d + 1 < dist[ni][nj]) {
+                    dist[ni][nj] = d + 1;
+                    pq.push({d + 1, {ni, nj}});
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+
+
+
+/* class Solution {
 public:
     bool isValid(int i, int j, int n, int m, vector<vector<int>>& g, vector<vector<bool>>& vis) {
         return i >= 0 && i < n && j >= 0 && j < m && g[i][j] == 0 && !vis[i][j];
@@ -40,3 +84,4 @@ public:
         return -1;
     }
 };
+ */
