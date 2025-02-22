c543538345281543aed1729c22ff1397d1d447fc
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-02-23 00:03:50 +0530

    Time: 16 ms (70.85%) | Memory: 24 MB (73.89%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,94 +1,42 @@
 class Solution {
 public:
-    
-    bool check(int i, int j, int n, int m,vector<vector<int>>& g,  vector<vector<bool>> &vis)
-    {
-        if(i>=n || j>=m |i<0 || j<0 || g[i][j]!=0 ||  vis[i][j])
-            return false;
-        return true;
+    bool isValid(int i, int j, int n, int m, vector<vector<int>>& g, vector<vector<bool>>& vis) {
+        return i >= 0 && i < n && j >= 0 && j < m && g[i][j] == 0 && !vis[i][j];
     }
-    
+
     int shortestPathBinaryMatrix(vector<vector<int>>& g) {
-        int n  = g.size();
-        int m = g[0].size();
-        
-        vector<vector<bool>> vis(n+1,vector<bool>(m+1,0));
-        int res = 0;
+        int n = g.size();
+        if (g[0][0] != 0 || g[n - 1][n - 1] != 0) return -1;
 
-        queue<pair<int,int>> q;
-        
-        if(g[0][0] == 0)
-        {
-            q.push({0,0});
-            vis[0][0]=1;
-            
-        }
-        
-        while(!q.empty())
-        {
+        vector<vector<bool>> vis(n, vector<bool>(n, false));
+        queue<pair<int, int>> q;
+        vector<pair<int, int>> directions = {
+            {1, 0}, {-1, 0}, {0, 1}, {0, -1},
+            {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
+        };
+
+        q.push({0, 0});
+        vis[0][0] = true;
+        int res = 1;
+
+        while (!q.empty()) {
             int sz = q.size();
-            res++;
-            
-            for(int ii=0;ii<sz;ii++)
-            {
-                auto p = q.front();
-                int i = p.first;
-                int j = p.second;
+            while (sz--) {
+                auto [i, j] = q.front();
                 q.pop();
-                
-                if(i==n-1 && j==m-1)
-                    return res;
 
- 
-                if(check(i+1,j,n,m,g,vis))
-                {
-                    q.push({i+1,j});
-                    vis[i+1][j]=1;
-                }
-                if(check(i-1,j,n,m,g,vis))
-                {
-                    q.push({i-1,j});
-                    vis[i-1][j]=1;
-                }
-                
-                if(check(i,j+1,n,m,g,vis))
-                {
-                    q.push({i,j+1});
-                    vis[i][j+1]=1;
-                }
-                if(check(i,j-1,n,m,g,vis))
-                {
-                    q.push({i,j-1});
-                    vis[i][j-1]=1;
-                }
-                
-                
-                
-                if(check(i+1,j+1,n,m,g,vis))
-                {
-                    q.push({i+1,j+1});
-                    vis[i+1][j+1]=1;
-                }
-                if(check(i+1,j-1,n,m,g,vis))
-                {
-                    q.push({i+1,j-1});
-                    vis[i+1][j-1]=1;
-                }
-                if(check(i-1,j+1,n,m,g,vis))
-                {
-                    q.push({i-1,j+1});
-                    vis[i-1][j+1]=1;
-                }
-                if(check(i-1,j-1,n,m,g,vis))
-                {
-                    q.push({i-1,j-1});
-                    vis[i-1][j-1]=1;
-                }
+                if (i == n - 1 && j == n - 1) return res;
 
-                
+                for (auto [di, dj] : directions) {
+                    int ni = i + di, nj = j + dj;
+                    if (isValid(ni, nj, n, n, g, vis)) {
+                        q.push({ni, nj});
+                        vis[ni][nj] = true;
+                    }
+                }
             }
+            res++;
         }
         return -1;
-        
     }
 };
