08b4963a7682d0d0791387a12e700308b8255122
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-03-15 00:59:50 +0530

    Time: 31 ms (90.19%) | Memory: 105.9 MB (30.13%) - LeetSync

---
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int left = 0;
+        long curr = 0;
+        
+        for (int right = 0; right < nums.size(); right++) {
+            long target = nums[right];
+            curr += target;
+            
+            if ((right - left + 1) * target - curr > k) {
+                curr -= nums[left];
+                left++;
+            }
+        }
+        
+        return nums.size() - left;
+    }
+};
+
+/* class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+       
+    sort(nums.begin(), nums.end());
+
+    long long int left = 0;
+    long long int maxFreq = 0;
+    long long sum = 0;
+
+    for (long long int right = 0; right < nums.size(); right++)
+    {
+        sum += nums[right];
+
+      
+        while ((right - left + 1) * nums[right]-sum > k )
+        {
+            sum -= nums[left];
+            left++;
+        }
+
+        maxFreq = max(maxFreq, right - left + 1);
+    }
+
+    return maxFreq; 
+    }
+}; */
