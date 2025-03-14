89101d440e774dd18fe98d2c64cf708eb170dddf
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-03-14 20:32:16 +0530

    Time: 69 ms (95.76%) | Memory: 79.2 MB (56.97%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
@@ -1,4 +1,20 @@
-class Solution {
+class Solution{
+public:
+    long long countGood(vector<int>& A, int k) {
+        long long res = 0;
+        unordered_map<int, int> count;
+        for (int left = 0, right = 0; right< A.size(); ++right) {
+            k -= count[A[right]]++;
+            while (k <= 0)
+                k += --count[A[left++]];
+            res += left;
+        }
+        return res;
+    }
+};
+
+
+/* class Solution {
 public:
     long long countGood(vector<int>& nums, int k) {
         unordered_map<int, int> freq;
@@ -21,3 +37,4 @@ public:
         return count;
     }
 };
+ */
