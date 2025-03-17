03e02d1d9cf019ad40880cbf6379bff27c13b234
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-03-17 22:52:28 +0530

    Time: 3 ms (48.46%) | Memory: 12.1 MB (25.86%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.cpp
+++ 273-integer-to-english-words/integer-to-english-words.cpp
@@ -1,5 +1,44 @@
 class Solution {
 public:
+    vector<pair<int, string>> numberToWordsMap = {
+        {1000000000, "Billion"}, {1000000, "Million"}, {1000, "Thousand"},
+        {100, "Hundred"}, {90, "Ninety"}, {80, "Eighty"}, {70, "Seventy"},
+        {60, "Sixty"}, {50, "Fifty"}, {40, "Forty"}, {30, "Thirty"},
+        {20, "Twenty"}, {19, "Nineteen"}, {18, "Eighteen"}, {17, "Seventeen"},
+        {16, "Sixteen"}, {15, "Fifteen"}, {14, "Fourteen"}, {13, "Thirteen"},
+        {12, "Twelve"}, {11, "Eleven"}, {10, "Ten"}, {9, "Nine"}, {8, "Eight"},
+        {7, "Seven"}, {6, "Six"}, {5, "Five"}, {4, "Four"}, {3, "Three"},
+        {2, "Two"}, {1, "One"}
+    };
+
+    string numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+
+        for (auto& [value, word] : numberToWordsMap) {
+            // Check if the number is greater than or equal to the current unit
+            if (num >= value) {
+                // Convert the quotient to words if the current unit is 100 or greater
+                string prefix = (num >= 100) ? numberToWords(num / value) + " " : "";
+
+                // Get the word for the current unit
+                string unit = word;
+
+                // Convert the remainder to words if it's not zero
+                string suffix = (num % value == 0) ? "" : " " + numberToWords(num % value);
+
+                return prefix + unit + suffix;
+            }
+        }
+
+        return "";
+    }
+};
+
+
+/* class Solution {
+public:
     const vector<string> belowTen = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
     const vector<string> belowTwenty = { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
     const vector<string> belowHundred = { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
@@ -36,4 +75,4 @@ private:
         }
         return convertToWords(num / 1000000000) + " Billion" + (num % 1000000000 ? " " + convertToWords(num % 1000000000) : "");
     }
-};
+}; */
