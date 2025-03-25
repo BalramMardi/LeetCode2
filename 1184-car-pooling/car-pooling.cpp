89d4b0953d55502288157dc16c186145aacae8a2
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-03-25 19:01:52 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (73.54%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,7 +1,18 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
+public:
+        bool carPooling(vector<vector<int>>& trips, int capacity) {
+        map<int, int> m;
+        for (auto &t : trips)
+            m[t[1]] += t[0], m[t[2]] -= t[0];
+        for (auto &v : m)
+            if ((capacity -= v.second) < 0)
+                return false;
+        return true;
+    }
+};
+
+
+/* class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
         vector<pair<int, int>> events;
@@ -24,7 +35,7 @@ public:
 
         return true;
     }
-};
+}; */
 
 
 
