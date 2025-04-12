f5f730cf8ece289b5f2ec3bff7bfb596f9b2780d
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-04-12 20:49:11 +0530

    Time: 154 ms (55.14%) | Memory: 11 MB (96.17%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+    int dp[305][305];
+
+    int solve(vector<int>& arr, int i, int j) {
+        if (i + 1 == j) return 0; // no balloon in between
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int maxCoins = 0;
+
+        for (int k = i + 1; k < j; k++) {
+            int coins = arr[i] * arr[k] * arr[j];
+            coins += solve(arr, i, k);
+            coins += solve(arr, k, j);
+            maxCoins = max(maxCoins, coins);
+        }
+
+        return dp[i][j] = maxCoins;
+    }
+
+    int maxCoins(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        return solve(nums, 0, nums.size() - 1);
+    }
+};
+
+
+/* class Solution {
+public:
     int maxCoins(vector<int>& nums) {
     int n = nums.size();
     
@@ -24,4 +54,4 @@ public:
     return dp[0][n + 1];
 }
 
-};
+}; */
