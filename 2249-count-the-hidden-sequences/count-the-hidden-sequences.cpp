63035b7fc96ec562e75a00fbed1ee65aacf298f7
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-04-21 10:50:37 +0530

    Time: 0 ms (100.00%) | Memory: 112.5 MB (97.58%) - LeetSync

--- 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    int numberOfArrays(vector<int>& differences, int lower, int upper) {
-        int x = 0, y = 0, cur = 0;
-        for (int d : differences) {
-            cur += d;
-            x = min(x, cur);
-            y = max(y, cur);
-            if (y - x > upper - lower) {
-                return 0;
-            }
+    int numberOfArrays(vector<int>& diff, int lower, int upper) {
+        long sum = 0, maxi = 0, mini = 0;
+        for (int i : diff) {
+            sum += i;
+            maxi = max(maxi, sum);
+            mini = min(mini, sum);
         }
-        return (upper - lower) - (y - x) + 1;
+        return max(0L, upper - lower - maxi + mini + 1);
     }
 };
