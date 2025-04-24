83dbc9558738badeaeb2319ac67a88aae0782e3b
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-04-24 10:56:55 +0530

    Time: 23 ms (51.27%) | Memory: 41.7 MB (81.93%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     int countCompleteSubarrays(vector<int>& nums) {
-        int res = 0;
-        unordered_map<int, int> cnt;
         int n = nums.size();
-        int right = 0;
-        unordered_set<int> distinct(nums.begin(), nums.end());
-        int distinct_count = distinct.size();
+        
+        int totalDistinct = unordered_set<int>(nums.begin(), nums.end()).size();
+        
+        unordered_map<int, int> freq; 
+        int res = 0;
+        int left = 0;
+        int distinctInWindow = 0;
 
-        for (int left = 0; left < n; left++) {
-            if (left > 0) {
-                int remove = nums[left - 1];
-                cnt[remove]--;
-                if (cnt[remove] == 0) {
-                    cnt.erase(remove);
-                }
+        for (int right = 0; right < n; ++right) {
+
+            if (++freq[nums[right]] == 1)
+                distinctInWindow++;
+
+            
+            while (distinctInWindow == totalDistinct) {
+                if (--freq[nums[left]] == 0)
+                    distinctInWindow--;
+                left++;
             }
-            while (right < n && cnt.size() < distinct_count) {
-                int add = nums[right];
-                cnt[add]++;
-                right++;
-            }
-            if (cnt.size() == distinct_count) {
-                res += (n - right + 1);
-            }
+
+            res += left;
         }
+
         return res;
     }
+
 };
