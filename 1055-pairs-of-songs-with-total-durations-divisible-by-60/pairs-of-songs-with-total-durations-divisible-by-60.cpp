ee23305a457a34d1d4e90febd893ee7a1bb0099a
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-05-05 13:38:55 +0530

    Time: 0 ms (100.00%) | Memory: 32 MB (74.06%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -4,19 +4,13 @@ public:
         vector<int> remainder(60, 0);
         int count = 0;
         
-        // Count the remainder frequencies
         for(int t : time) {
             int rem = t % 60;
-            
-            // If the remainder is 0, look for other songs with remainder 0
-            if (rem == 0) {
-                count += remainder[0];
-            } else {
-                // Otherwise, look for songs that complement this remainder to make 60
-                count += remainder[60 - rem];
-            }
-            
-            // Increment the remainder frequency
+
+            int complement = (rem == 0) ? 0 : 60-rem;
+
+            count += remainder[complement];
+
             remainder[rem]++;
         }
 
