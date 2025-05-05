b1eec468ca9409f208ba6c3dea1f07ac34e6a72d
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-05-05 19:33:16 +0530

    Time: 4 ms (16.59%) | Memory: 8.6 MB (22.42%) - LeetSync

---
+++ 1238-alphabet-board-path/alphabet-board-path.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    string alphabetBoardPath(string target) {
+        unordered_map<char,pair<int,int>> mp;
+
+        char ch = 'a';
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                mp[ch]={i,j};
+                ch = ch+1;
+            }
+        }
+        mp['z']={5,0};
+
+        string ans = "";
+
+        int startX = 0,startY=0;
+        for(int i=0;i<target.size();i++){
+            char ch = target[i];
+            int endX = mp[ch].first;
+            int endY = mp[ch].second;
+
+            int diffX = startX - endX;
+            int diffY = startY-endY;
+
+            if(ch!='z'){
+                if(diffX > 0 ){
+                ans += string(diffX , 'U');
+                }
+                else if(diffX < 0){
+                    ans += string(abs(diffX) , 'D');
+                }
+
+                if(diffY > 0 ){
+                    ans += string(diffY , 'L');
+                }
+                else if(diffY < 0){
+                    ans += string(abs(diffY) , 'R');
+                }
+
+                ans += "!";
+
+                startX = endX;
+                startY = endY;
+            }
+            else{
+                if(diffY > 0 ){
+                    ans += string(diffY , 'L');
+                }
+                else if(diffY < 0){
+                    ans += string(abs(diffY) , 'R');
+                }
+
+                if(diffX > 0 ){
+                ans += string(diffX , 'U');
+                }
+                else if(diffX < 0){
+                    ans += string(abs(diffX) , 'D');
+                }
+
+                ans += "!";
+
+                startX = endX;
+                startY = endY;
+            }
+
+            
+            
+        }
+
+        return ans;
+    }
+};
