892daf0df3764ec52f73e36fe9badc8fa3ed3872
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-05-05 11:29:55 +0530

    Time: 7 ms (73.33%) | Memory: 14.1 MB (59.52%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+    int partitionString(string str) {
+          vector<int> freq(26, 0);
+        int counter = 1;
+        for (int i = 0; i < str.size(); i++)
+        {
+            char ch = str[i];
+            if (freq[ch - 'a'] == 1)
+            {
+                counter++;
+                freq.assign(26, 0);
+            }
+            freq[ch - 'a']++;
+        }
+        return counter;
+    }
+};
+
+
+/* class Solution {
+public:
     int partitionString(string s) {
           vector<int>ans(26,0) ;
           int count  = 1;
@@ -15,4 +35,4 @@ public:
           }
           return count ;
     }
-};
+}; */
