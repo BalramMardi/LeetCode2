a127e019e5c81f5bc1ac900930a56565e8a79ef4
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-05-07 20:59:43 +0530

    Time: 3 ms (67.09%) | Memory: 9.9 MB (68.00%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -2,6 +2,34 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         if (s1.size() > s2.size()) return false;
+
+        vector<int> s1Count(26, 0), s2Count(26, 0);
+
+        for (char c : s1) s1Count[c - 'a']++;
+
+        int window = s1.size();
+        for (int i = 0; i < s2.size(); ++i) {
+            s2Count[s2[i] - 'a']++;
+
+            // Shrink window if needed
+            if (i >= window) {
+                s2Count[s2[i - window] - 'a']--;
+            }
+
+            // Compare counts
+            if (s1Count == s2Count) return true;
+        }
+
+        return false;
+    }
+};
+
+
+
+/* class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size()) return false;
         vector<int> s1v (26, 0);
         for (auto c : s1) s1v[c - 'a']++;
         vector<int> s2v (26, 0);
@@ -19,4 +47,4 @@ public:
         }
         return false;
     }
-};
+}; */
