02c87d513a69c44ec3c2a4325eacd8fd23dd13f1
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-05-17 22:15:25 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (48.91%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,36 @@
+class Solution {
+
+public:
+void swap(vector<int>& nums,int i,int j){
+    int temp=nums[i];
+    nums[i]=nums[j];
+    nums[j]=temp;
+    
+}
+
+
+public:
+    void sortColors(vector<int>& nums) {
+        int mid=0,low=0,high=nums.size()-1;
+        
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums,low,mid);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else if(nums[mid]==2){
+                swap(nums,mid,high);
+                high--;
+            }
+        }
+
+        
+        for(auto i : nums){
+            cout<<i;
+        }
+    }
+};
