beafe65c81c8b2328fd36bb4ade3ee112e2ea5dc
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-06-08 10:52:35 +0530

    Time: 71 ms (16.91%) | Memory: 38.4 MB (5.05%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,4 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TrieNode {
+public:
+    unordered_map<int, TrieNode*> children;
+    bool isEnd = false;
+};
+
 class Solution {
+    TrieNode* root;
+
+    void insert(int num) {
+        TrieNode* node = root;
+        string s = to_string(num);
+        for (char c : s) {
+            int digit = c - '0';
+            if (!node->children.count(digit))
+                node->children[digit] = new TrieNode();
+            node = node->children[digit];
+        }
+        node->isEnd = true;
+    }
+
+    void dfs(TrieNode* node, string current, vector<int>& result) {
+        if (node->isEnd) {
+            result.push_back(stoi(current));
+        }
+        for (int i = 0; i <= 9; ++i) {
+            if (node->children.count(i)) {
+                dfs(node->children[i], current + to_string(i), result);
+            }
+        }
+    }
+
+public:
+    vector<int> lexicalOrder(int n) {
+        root = new TrieNode();
+        for (int i = 1; i <= n; ++i) {
+            insert(i);
+        }
+
+        vector<int> result;
+        for (int i = 1; i <= 9; ++i) {
+            if (root->children.count(i)) {
+                dfs(root->children[i], to_string(i), result);
+            }
+        }
+        return result;
+    }
+};
+
+
+/* class Solution {
 public:
     vector<int> lexicalOrder(int n) {
         vector<string> str;
@@ -19,4 +72,4 @@ public:
 
         return ans;
     }
-};
+}; */
