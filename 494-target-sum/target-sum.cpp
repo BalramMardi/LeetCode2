0d6e8d590200c0b399ce366b29b0b5c2ddffee6b
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-06-10 15:07:11 +0530

    Time: 1 ms (91.49%) | Memory: 11.8 MB (75.24%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        return backtrack(nums, target, 0, 0);
+    int dp[21][2001];
+
+    int solve(vector<int>& nums, int i, int sum, int target) {
+        if (i == nums.size()) return sum == target;
+        if (dp[i][sum + 1000] != -1) return dp[i][sum + 1000];
+
+        int plus = solve(nums, i + 1, sum + nums[i], target);
+        int minus = solve(nums, i + 1, sum - nums[i], target);
+        return dp[i][sum + 1000] = plus + minus;
     }
-private:
-    
-    map<pair<int, int>, int> dp;
-    
-    int backtrack(vector<int>& nums, int target, int i, int total) {
-        if (i == nums.size()) {
-            return total == target ? 1 : 0;
-        }
-        if (dp.find({i, total}) != dp.end()) {
-            return dp[{i, total}];
-        }
-        
-        dp[{i, total}] = backtrack(nums, target, i + 1, total + nums[i])
-                       + backtrack(nums, target, i + 1, total - nums[i]);
-        
-        return dp[{i, total}];
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        memset(dp, -1, sizeof(dp));
+        return solve(nums, 0, 0, target);
     }
 };
 
