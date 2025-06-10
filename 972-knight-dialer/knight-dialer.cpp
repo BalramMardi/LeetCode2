3e2a9db8699255dd5ea3f9699603db50aa29a7fc
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-06-10 12:02:15 +0530

    Time: 39 ms (79.43%) | Memory: 8.9 MB (86.54%) - LeetSync

--- 972-knight-dialer/knight-dialer.cpp
+++ 972-knight-dialer/knight-dialer.cpp
@@ -1,4 +1,47 @@
+const int MOD = 1e9 + 7;
+
+vector<vector<int>> moves = {
+    {4, 6},     // 0
+    {6, 8},     // 1
+    {7, 9},     // 2
+    {4, 8},     // 3
+    {0, 3, 9},  // 4
+    {},         // 5 (knight cannot move to 5)
+    {0, 1, 7},  // 6
+    {2, 6},     // 7
+    {1, 3},     // 8
+    {2, 4}      // 9
+};
+
 class Solution {
+public:
+    int dp[5001][10];
+
+    int solve(int n, int digit) {
+        if (n == 1) return 1; 
+        if (dp[n][digit] != -1) return dp[n][digit];
+
+        long long ans = 0;
+        for (int next : moves[digit]) {
+            ans = (ans + solve(n - 1, next)) % MOD;
+        }
+        return dp[n][digit] = ans;
+    }
+
+    int knightDialer(int n) {
+        memset(dp, -1, sizeof(dp));
+        long long res = 0;
+
+        for (int i = 0; i <= 9; i++) {
+            res = (res + solve(n, i)) % MOD;
+        }
+
+        return res;
+    }
+};
+
+
+/* class Solution {
 public:
     int knightDialer(int n) {
         vector<vector<int>> moves = {
@@ -42,3 +85,4 @@ public:
         return result;
     }
 };
+ */
