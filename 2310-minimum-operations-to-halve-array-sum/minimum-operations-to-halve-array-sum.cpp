bcb7a3d98859a80709a9b51d129a96d9ac2a4bba
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-06-22 01:01:22 +0530

    Time: 88 ms (45.57%) | Memory: 93.2 MB (33.25%) - LeetSync

--- 2310-minimum-operations-to-halve-array-sum/minimum-operations-to-halve-array-sum.cpp
+++ 2310-minimum-operations-to-halve-array-sum/minimum-operations-to-halve-array-sum.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     int halveArray(vector<int>& nums) {
+        double s = accumulate(begin(nums), end(nums), 0.0), k = 0;
+        int i = 0;
+        priority_queue<double> pq;
+        
+        for (int num : nums)
+            pq.push((double)num);
+
+        while (s - k > s / 2) {
+            double x = pq.top(); pq.pop();
+            k += x / 2;
+            pq.push(x / 2);     
+            i++;
+        }
+
+        return i;
+    }
+
+};
+
+
+/* class Solution {
+public:
+    int halveArray(vector<int>& nums) {
         priority_queue<double> maxHeap;
         double totalSum = 0;
 
@@ -26,3 +49,4 @@ public:
         return operations;
     }
 };
+ */
