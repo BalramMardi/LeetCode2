35a610d8d443c0be1ddcdadae86e9476f96f75ac
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-06-23 23:53:16 +0530

    Time: 0 ms (100.00%) | Memory: 35.7 MB (99.06%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int low = 0, high = arr.size() - k;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (x - arr[mid] > arr[mid + k] - x)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        return vector<int>(arr.begin() + low, arr.begin() + low + k);
+    }
+};
+
+
+
+/* class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         priority_queue<pair<int, int>> maxHeap;
 
         for (int num : arr) {
@@ -17,7 +34,7 @@ public:
         sort(res.begin(), res.end());
         return res;
     }
-};
+}; */
 
 
 /* class Solution {
