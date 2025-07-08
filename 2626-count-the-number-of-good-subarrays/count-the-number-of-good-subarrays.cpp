0f164c52f4bfd699d63083bd2b4096e436ef3387
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-07-08 23:58:37 +0530

    Time: 75 ms (79.07%) | Memory: 79 MB (73.29%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
@@ -1,4 +1,29 @@
-class Solution{
+class Solution {
+public:
+    long long countGood(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        long long count = 0, pairs = 0;
+        int left = 0;
+
+        for (int right = 0; right < nums.size(); ++right) {
+          
+            pairs += freq[nums[right]];
+            freq[nums[right]]++;
+
+      
+            while (pairs >= k) {
+                count += (nums.size() - right);
+                freq[nums[left]]--;
+                pairs -= freq[nums[left]];
+                left++;
+            }
+        }
+        return count;
+    }
+};
+
+
+/* class Solution{
 public:
     long long countGood(vector<int>& A, int k) {
         long long res = 0;
@@ -11,30 +36,6 @@ public:
         }
         return res;
     }
-};
+}; */
  
 
-/* class Solution {
-public:
-    long long countGood(vector<int>& nums, int k) {
-        unordered_map<int, int> freq;
-        long long count = 0, current_pairs = 0;
-        int left = 0;
-
-        for (int right = 0; right < nums.size(); ++right) {
-          
-            current_pairs += freq[nums[right]];
-            freq[nums[right]]++;
-
-      
-            while (current_pairs >= k) {
-                count += (nums.size() - right);
-                freq[nums[left]]--;
-                current_pairs -= freq[nums[left]];
-                left++;
-            }
-        }
-        return count;
-    }
-};
- */
