69a7066aeaf8ad489cd476169825e091990f4b26
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-07-09 01:22:13 +0530

    Time: 0 ms (100.00%) | Memory: 60.2 MB (22.89%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
+        
+        int n=nums.size();
+        int zero=0;
+
+        int left=0;
+
+        int maxi = 0;
+
+        for(int right=0;right<n;right++){
+
+            if(nums[right]==0){
+                zero++;
+            }
+
+            while(zero > 1){
+                if(nums[left]==0){
+                    zero--;
+                }
+                left++;
+            }
+
+            maxi = max(maxi, right-left);
+        }
+
+        return maxi;
+        
+    }
+};
+
+
+/* class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
         int count=0;
         int maxx=0;
         int backup=0;
@@ -19,4 +52,4 @@ public:
         return maxx == nums.size() ? maxx-1 : maxx;
         
     }
-};
+}; */
