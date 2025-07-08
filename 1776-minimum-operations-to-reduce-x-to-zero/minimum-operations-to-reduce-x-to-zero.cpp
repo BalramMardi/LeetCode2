78299f865c1df5b1acf85fdb95cdc8f36fa122dc
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-07-09 00:08:18 +0530

    Time: 0 ms (100.00%) | Memory: 102.2 MB (90.77%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,3 +1,31 @@
+/* class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {        
+        int n    = nums.size();
+        int sum  = 0;
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        for(int i = 0; i<n; i++) {
+            sum += nums[i];
+            mp[sum] = i;
+        }
+        if(sum < x)
+            return -1;
+        int restSum  = sum-x;
+        int longest  = INT_MIN;
+        sum = 0;
+        for(int i = 0; i<n; i++) {
+            sum += nums[i];
+            if(mp.count(sum-restSum)) {
+                longest = max(longest, i-mp[sum-restSum]);
+            }
+        }
+        
+        return longest==INT_MIN?-1:n-longest;
+    }
+}; */
+
+
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
@@ -8,14 +36,14 @@ public:
 
         int l = 0, mini = INT_MAX, current_sum = 0;
 
-        for (int r = 0; r < n;) {
-            current_sum += nums[r++];               
+        for (int r = 0; r < n;r++) {
+            current_sum += nums[r];               
             while(l<n && current_sum > target)       
             {
                 current_sum -= nums[l++];
             }
             if(current_sum == target)
-            mini = min(mini, n-(r-l));      
+                mini = min(mini, n-(r-l+1));      
         }
         return mini == INT_MAX ? -1 : mini;
     }
