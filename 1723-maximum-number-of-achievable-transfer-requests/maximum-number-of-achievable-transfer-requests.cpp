23b08c643095344c64d7de58ef3ba46da2ac0251
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-07-14 16:23:25 +0530

    Time: 498 ms (19.19%) | Memory: 185.4 MB (21.09%) - LeetSync

---
+++ 1723-maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+        int m = requests.size();
+        int max_achievable = 0;
+
+        for (int mask = 0; mask < (1 << m); ++mask) {
+            vector<int> degree(n, 0);
+            int current_requests = 0;
+
+            for (int i = 0; i < m; ++i) {
+                if ((mask >> i) & 1) {
+                    degree[requests[i][0]]--;
+                    degree[requests[i][1]]++;
+                    current_requests++;
+                }
+            }
+
+            bool possible = true;
+            for (int i = 0; i < n; ++i) {
+                if (degree[i] != 0) {
+                    possible = false;
+                    break;
+                }
+            }
+
+            if (possible) {
+                max_achievable = max(max_achievable, current_requests);
+            }
+        }
+
+        return max_achievable;
+    }
+};
