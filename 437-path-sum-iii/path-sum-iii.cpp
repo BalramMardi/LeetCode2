5b3fd91abd62e2361ce22b0984610a4940e83c3b
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-08-31 20:51:28 +0530

    Time: 10 ms (46.51%) | Memory: 20.6 MB (45.36%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -1,4 +1,33 @@
+class Solution {
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        unordered_map<long long, int> prefix; 
+        prefix[0] = 1; 
+        return dfs(root, 0, targetSum, prefix);
+    }
 
+    int dfs(TreeNode* node, long long currSum, int target, unordered_map<long long,int>& prefix) {
+        if (!node) return 0;
+
+        currSum += node->val;
+        int count = 0;
+
+        if (prefix.count(currSum - target)) {
+            count += prefix[currSum - target];
+        }
+
+        prefix[currSum]++;
+
+        count += dfs(node->left, currSum, target, prefix);
+        count += dfs(node->right, currSum, target, prefix);
+
+        prefix[currSum]--; 
+
+        return count;
+    }
+};
+
+/* 
 class Solution {
 public:
     int pathSum(TreeNode* root, int targetSum) {
@@ -24,3 +53,4 @@ public:
         return cnt;
     }
 };
+ */
