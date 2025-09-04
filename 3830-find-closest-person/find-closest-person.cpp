d43420a602f736555954ceb81054fd2275a87a3a
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-09-04 12:47:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (55.11%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int dif1 = abs(z-x);
+        int dif2 = abs(z-y);
+
+        if(dif1 > dif2){
+            return 2;
+        }
+        else if(dif1 < dif2){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+};
