dcf4507fbed01cf31570b0daa73aa6b222697961
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-09-10 01:16:49 +0530

    Time: 21 ms (83.89%) | Memory: 45.4 MB (85.14%) - LeetSync

--- 1140-distant-barcodes/distant-barcodes.cpp
+++ 1140-distant-barcodes/distant-barcodes.cpp
@@ -1,6 +1,46 @@
 class Solution {
 public:
     vector<int> rearrangeBarcodes(vector<int>& barcodes) {
+        unordered_map<int,int> freq;
+        for (int b : barcodes) freq[b]++;
+        
+        vector<pair<int,int>> arr(freq.begin(), freq.end());
+        sort(arr.begin(), arr.end(), [](auto &a, auto &b){
+            return a.second > b.second;
+        });
+        
+        int n = barcodes.size();
+        vector<int> res(n);
+        int idx = 0;
+        
+        for (auto &p : arr) {
+            int val = p.first, count = p.second;
+            while (count > 0 && idx < n) {
+                res[idx] = val;
+                idx += 2;
+                count--;
+            }
+            p.second = count; 
+        }
+        
+        idx = 1;
+        for (auto &p : arr) {
+            int val = p.first, count = p.second;
+            while (count > 0 && idx < n) {
+                res[idx] = val;
+                idx += 2;
+                count--;
+            }
+        }
+        
+        return res;
+    }
+};
+
+
+/* class Solution {
+public:
+    vector<int> rearrangeBarcodes(vector<int>& barcodes) {
          unordered_map<int,int> freq;
         for (int b : barcodes) freq[b]++;
 
@@ -12,7 +52,6 @@ public:
             auto [cnt1, val1] = pq.top(); pq.pop();
             auto [cnt2, val2] = pq.top(); pq.pop();
 
-            // Place them alternately
             res.push_back(val1);
             res.push_back(val2);
 
@@ -24,4 +63,4 @@ public:
 
         return res;
     }
-};
+}; */
