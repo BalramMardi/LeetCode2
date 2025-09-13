590b4be398cf510a6f238c2cf179a2452adc1994
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-09-13 19:47:32 +0530

    Time: 55 ms (27.92%) | Memory: 79.6 MB (36.04%) - LeetSync

--- 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
@@ -1,6 +1,44 @@
 class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
+        typedef pair<int,int> pii; 
+        priority_queue<pii, vector<pii>, greater<pii>> pq;
+
+        int n = costs.size();
+        int l = 0, r = n - 1;
+
+        for (int i = 0; i < candidates && l <= r; i++) {
+            pq.push({costs[l], l});
+            l++;
+        }
+        for (int i = 0; i < candidates && l <= r; i++) {
+            pq.push({costs[r], r});
+            r--;
+        }
+
+        long long ans = 0;
+        while (k--) {
+            auto [cost, idx] = pq.top();
+            pq.pop();
+            ans += cost;
+
+            if (idx < l && l <= r) { 
+                pq.push({costs[l], l});
+                l++;
+            } 
+            else if (idx > r && l <= r) { 
+                pq.push({costs[r], r});
+                r--;
+            }
+        }
+        return ans;
+    }
+};
+
+
+/* class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
         int n = costs.size();
         long long ans = 0;
 
@@ -15,12 +53,14 @@ public:
                 auto [cost, idx] = right.top(); right.pop();
                 ans += cost;
                 if (j >= i) right.push({costs[j], j--});
-            } else {
+            } 
+            else {
                 auto [cost, idx] = left.top(); left.pop();
                 ans += cost;
                 if (i <= j) left.push({costs[i], i++});
             }
+
         }
         return ans;
     }
-};
+}; */
