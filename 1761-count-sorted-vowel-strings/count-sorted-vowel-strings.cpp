bce652357d9b589195d23f3aa6e1a151ed806d54
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-09-22 13:28:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (62.50%) - LeetSync

--- 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    int dp[55][6];
+
+    int solve(int n, int idx) {
+        if (n == 0) return 1;          
+        if (idx == 5) return 0;        
+        if (dp[n][idx] != -1) return dp[n][idx];
+
+        int take = solve(n - 1, idx);   
+        int skip = solve(n, idx + 1);
+
+        return dp[n][idx] = take + skip;
+    }
+
+    int countVowelStrings(int n) {
+        memset(dp, -1, sizeof(dp));
+        return solve(n, 0);
+    }
+};
+
+/* class Solution {
+public:
     int countVowelStrings(int n) {
         long long res = 1;
         
@@ -8,4 +29,4 @@ public:
         }
         return (int)res;
     }
-};
+}; */
