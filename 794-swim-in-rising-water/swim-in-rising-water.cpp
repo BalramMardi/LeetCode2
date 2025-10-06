cc5ad75bea12c27e8ab607ab3863cebf9093aa1a
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-10-06 20:03:50 +0530

    Time: 3 ms (88.24%) | Memory: 13.3 MB (63.89%) - LeetSync

---
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+typedef pair<int, pair<int, int>> iPair;
+
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+    priority_queue<iPair, vector<iPair>, greater<iPair>> pq;
+    vector<vector<int>> vis(n, vector<int>(n, false));
+
+    pq.push({grid[0][0], {0, 0}});
+
+    while (pq.size())
+    {
+        iPair rem = pq.top();
+        pq.pop();
+
+        if (rem.second.first == n - 1 and rem.second.second == n - 1)
+        {
+            return rem.first;
+        }
+
+        if (vis[rem.second.first][rem.second.second])
+        {
+            continue;
+        }
+        vis[rem.second.first][rem.second.second] = true;
+
+        int dirs[4][2] = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+
+        for (int i = 0; i < 4; i++)
+        {
+            int rowd = rem.second.first + dirs[i][0];
+            int cold = rem.second.second + dirs[i][1];
+
+            if (rowd < 0 or cold < 0 or rowd >= n or cold >= n or vis[rowd][cold] == true)
+            {
+                continue;
+            }
+            pq.push({max(rem.first, grid[rowd][cold]), {rowd, cold}});
+        }
+    }
+
+    return 0;
+    }
+};
