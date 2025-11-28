b56f1f48f48309c3c177e7e589789fafe629a1b5
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-11-28 11:39:23 +0530

    Time: 31 ms (28.60%) | Memory: 11 MB (55.42%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int n=s.length();
-        vector<vector<bool>> dp(n,vector<bool>(n,false));
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int count = 0;
 
-        int count=0;
-        for(int g=0;g<n;g++){
-            for(int i=0,j=g;j<n;j++,i++){
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                
+                if (len == 1) {
+                    dp[i][j] = true;
+                } else if (len == 2) {
+                    dp[i][j] = (s[i] == s[j]);
+                } else {
+                    dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]);
+                }
 
-                if(g==0){
-                    dp[i][j]=true;
-                    count++;
-                }
-                else if(g==1){
-                    if(s[i] == s[j]){
-                        dp[i][j] = true;
-                        count++;
-                    }
-                    else{
-                        dp[i][j]=false;
-                    }
-                }
-                else{
-                    if(s[i] == s[j] and dp[i+1][j-1]==true){
-                        dp[i][j] = true;
-                        count++;
-                    }
-                    else{
-                        dp[i][j]=false;
-                    }
-                }
+                if (dp[i][j]) count++;
             }
         }
         return count;
-
     }
 };
