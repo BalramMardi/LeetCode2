0f294f6ac89e05dbbff690357f3fe0e0e6675d7b
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-11-28 20:24:55 +0530

    Time: 1 ms (77.77%) | Memory: 10.6 MB (98.85%) - LeetSync

--- 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
     
-    int solve(vector<int>&arr, map<pair<int,int>,int>&maxi,int left,int right,vector<vector<int>> &dp){
-        if(left == right){
-            return 0;
-        }
-
-        if(dp[left][right]!=-1) return dp[left][right];
-        int ans = INT_MAX;
-        for(int k=left;k<right;k++){
-            ans = min(ans,maxi[{left,k}]* maxi[{k+1,right}]+solve(arr,maxi,left,k,dp)+solve(arr,maxi,k+1,right,dp));
-        }
-
-        return dp[left][right] = ans;
-    }
-
-
     int mctFromLeafValues(vector<int>& arr) {
-        int n=arr.size();
-        map<pair<int,int>,int> maxi;
+        int n = arr.size();
+        int dp[n][n];
+        int maxVal[n][n];
 
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        
-        for(int i=0;i<n;i++){
-            int mm = arr[i];
-            for(int j=i;j<n;j++){
-                mm = max(mm,arr[j]);
-                maxi[{i,j}]=mm;
+        for (int i = 0; i < n; i++) {
+            maxVal[i][i] = arr[i];
+            for (int j = i + 1; j < n; j++) {
+                maxVal[i][j] = max(maxVal[i][j - 1], arr[j]);
             }
         }
 
-        return solve(arr,maxi,0,n-1,dp);
+        for (int i = 0; i < n; i++) dp[i][i] = 0;
 
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                dp[i][j] = INT_MAX;
 
+                for (int k = i; k < j; k++) {
+                    int cost = dp[i][k] + dp[k + 1][j] + (maxVal[i][k] * maxVal[k + 1][j]);
+                    if (cost < dp[i][j]) {
+                        dp[i][j] = cost;
+                    }
+                }
+            }
+        }
+
+        return dp[0][n - 1];
     }
 };
