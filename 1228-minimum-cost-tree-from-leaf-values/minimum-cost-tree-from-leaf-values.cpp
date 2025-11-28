1cb101590ffb511ca28c8f1f2910900d75aa89c9
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-11-28 20:49:59 +0530

    Time: 5 ms (61.52%) | Memory: 10.8 MB (89.40%) - LeetSync

--- 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     
     int mctFromLeafValues(vector<int>& arr) {
-        int n = arr.size();
+         int n = arr.size();
         int dp[n][n];
-        int maxVal[n][n];
-
-        for (int i = 0; i < n; i++) {
-            maxVal[i][i] = arr[i];
-            for (int j = i + 1; j < n; j++) {
-                maxVal[i][j] = max(maxVal[i][j - 1], arr[j]);
-            }
-        }
 
         for (int i = 0; i < n; i++) dp[i][i] = 0;
 
         for (int len = 2; len <= n; len++) {
-            for (int i = 0; i < n-len+1; i++) {
+            for (int i = 0; i + len - 1 < n; i++) {
                 int j = i + len - 1;
                 dp[i][j] = INT_MAX;
 
                 for (int k = i; k < j; k++) {
-                    int cost = dp[i][k] + dp[k + 1][j] + (maxVal[i][k] * maxVal[k + 1][j]);
-                    dp[i][j]=min(cost,dp[i][j]);
+
+                    int leftMax = 0;
+                    for (int x = i; x <= k; x++)
+                        leftMax = max(leftMax, arr[x]);
+
+                    int rightMax = 0;
+                    for (int x = k + 1; x <= j; x++)
+                        rightMax = max(rightMax, arr[x]);
+
+                    int cost = dp[i][k] + dp[k+1][j] + leftMax * rightMax;
+                    dp[i][j] = min(dp[i][j], cost);
                 }
             }
         }
