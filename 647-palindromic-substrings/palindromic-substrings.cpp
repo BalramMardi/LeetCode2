db32fc6927521c89466b95a5c92c307361ca1576
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2025-11-28 11:42:17 +0530

    Time: 35 ms (24.57%) | Memory: 11.2 MB (43.48%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -6,7 +6,7 @@ public:
         int count = 0;
 
         for (int len = 1; len <= n; len++) {
-            for (int i = 0; i <= n - len; i++) {
+            for (int i = 0; i < n - len+1; i++) {
                 int j = i + len - 1;
                 
                 if (len == 1) {
