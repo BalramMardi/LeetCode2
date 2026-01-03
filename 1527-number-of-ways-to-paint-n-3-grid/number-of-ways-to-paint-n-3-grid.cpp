8658a63bda955c463cb6d2322befc48fba3ab5e1
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2026-01-03 20:41:53 +0530

    Time: 47 ms (28.00%) | Memory: 11.7 MB (32.33%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -1,17 +1,52 @@
 class Solution {
 public:
-    int numOfWays(int n) {
-        long long typeA = 6;
-        long long typeB = 6;
-        long long mod = 1e9 + 7;
-        
-        for (int i = 2; i <= n; i++) {
-            long long nextA = (2 * typeA + 2 * typeB) % mod;
-            long long nextB = (2 * typeA + 3 * typeB) % mod;
-            typeA = nextA;
-            typeB = nextB;
+
+    typedef long long ll;
+
+    ll dp[5001][4][4][4];
+    int mod = 1e9+7;
+    int N;
+    vector<int> choice = {1,2,3};
+
+    ll solve(int idx, int p1, int p2, int p3){
+
+        if(idx==N){
+            return 1;
         }
+
+        if(dp[idx][p1][p2][p3] != -1){
+            return dp[idx][p1][p2][p3];
+        }
+
+        ll count=0;
         
-        return (typeA + typeB) % mod;
+        for(int &c1 : choice){
+
+            if(c1==p1) continue;
+
+            for(int &c2 : choice){
+
+                if(c2==p2 || c2==c1) continue;
+
+                for(int &c3 : choice){
+
+                    if(c3==p3 || c3==c2) continue;
+
+                    count += solve(idx+1,c1,c2,c3) % mod;
+
+                }
+
+            }
+
+            
+        }
+
+        return dp[idx][p1][p2][p3] = count%mod;
+    }
+
+    int numOfWays(int n) {
+        N=n;
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,0,0);
     }
 };
