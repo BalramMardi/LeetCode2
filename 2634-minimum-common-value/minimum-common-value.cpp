09103dd519818179239d25e63ec550c20d570a1d
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2026-05-19 08:08:34 +0530

    Time: 39 ms (22.80%) | Memory: 69.6 MB (14.31%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> set1(nums1.begin(), nums1.end());
+        for (int it : nums2) {
+            if (set1.contains(it)) {
+                return it;
+            }
+        }
+
+        return -1;
+    }
+};
