cd3318a431b7237d548d6420c3caf4c52ba82db7
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2026-08-18 23:44:52 +0530

    Time: 1241 ms (50.51%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,3 +1,5 @@
-# Write your MySQL query statement below
-select p.product_name as product_name, s.year as year, s.price as price
-from Sales as s join Product as p on s.product_id=p.product_id
+-- Write your PostgreSQL query statement below
+select p.product_name, s.year, s.price
+from Sales as s
+left join Product as p
+on p.product_id=s.product_id
