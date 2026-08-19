6499fcf18f7ce8879f5fdbb44fe9cffe2ac528d5
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2026-08-19 10:21:57 +0530

    Time: 169 ms (68.61%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,7 +1,9 @@
-# Write your MySQL query statement below
-select a1.machine_id, round(avg(a2.timestamp-a1.timestamp), 3) as processing_time 
-from Activity a1
-join Activity a2 
-on a1.machine_id=a2.machine_id and a1.process_id=a2.process_id
-and a1.activity_type='start' and a2.activity_type='end'
-group by a1.machine_id
+-- Write your PostgreSQL query statement below
+select a.machine_id, round(avg(b.timestamp-a.timestamp)::numeric,3) as processing_time
+from Activity a
+join Activity b
+on a.machine_id = b.machine_id
+    and a.process_id = b.process_id
+where a.activity_type='start'
+    and b.activity_type='end'
+group by a.machine_id
