271beb257b30e2a5730bfa40cc006146b2a68b13
Balram Mardi <120470787+BalramMardi@users.noreply.github.com>
2026-08-21 14:53:24 +0530

    Time: 1064 ms (27.12%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,7 +1,10 @@
-# Write your MySQL query statement below
-
-
-SELECT contest_id, ROUND((COUNT(distinct user_id))*100/(SELECT COUNT(user_id) FROM Users),2) as percentage
-FROM Register
-GROUP BY contest_id
-ORDER BY percentage DESC, contest_id ASC
+-- Write your PostgreSQL query statement below
+SELECT
+    r.contest_id,
+    ROUND(
+        COUNT(r.user_id) * 100.0 / (SELECT COUNT(*) FROM Users),
+        2
+    ) AS percentage
+FROM Register AS r
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id;
